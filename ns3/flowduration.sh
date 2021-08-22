08d6785b3f1d564e03b1bc5d28aafb3faa7e5294
nmaksic <62266000+nmaksic@users.noreply.github.com>
2021-08-22 17:23:22 +0200

    Support for flow monitor results added

--- ns3/flowduration.sh
+++ ns3/flowduration.sh
@@ -1,10 +1,12 @@
 
 
+
 OLDIS=10
 TOPOLOGY=$1
 
 rm flowduration*.txt
 rm flowcount*.txt
+rm results*.xml
 
 for RUNNUMBER in 1 2 3 4 5
 do
@@ -16,18 +18,25 @@ do
    rm flowcount1.txt
    cp flowduration.txt flowduration1.txt
    cp flowcount.txt flowcount1.txt
+   cp results.xml results1.xml
+   cp throughputs.txt throughputs1.txt
+   
 
    ./waf --run "scratch/timpsim --flowecmp=false --routing=1 --oldicount=$OLDIS --topology=$TOPOLOGY --RngRun=$RUNNUMBER"
    rm flowduration2.txt
    rm flowcount2.txt
    cp flowduration.txt flowduration2.txt
    cp flowcount.txt flowcount2.txt
+   cp results.xml results2.xml
+   cp throughputs.txt throughputs2.txt
 
    ./waf --run "scratch/timpsim --flowecmp=false --routing=2 --oldicount=$OLDIS --topology=$TOPOLOGY --RngRun=$RUNNUMBER"
    rm flowduration3.txt
    rm flowcount3.txt
    cp flowduration.txt flowduration3.txt
    cp flowcount.txt flowcount3.txt
+   cp results.xml results3.xml
+   cp throughputs.txt throughputs3.txt
 
    if [ $1 -eq 4 ] || [ $1 -eq 5 ];
    then
@@ -37,12 +46,16 @@ do
    rm flowcount4.txt
    cp flowduration.txt flowduration4.txt
    cp flowcount.txt flowcount4.txt
+   cp results.xml results4.xml
+   cp throughputs.txt throughputs4.txt
 
    ./waf --run "scratch/timpsim --flowecmp=false --routing=4 --oldicount=$OLDIS --topology=$TOPOLOGY --RngRun=$RUNNUMBER"
    rm flowduration5.txt
    rm flowcount5.txt
    cp flowduration.txt flowduration5.txt
    cp flowcount.txt flowcount5.txt
+   cp results.xml results5.xml
+   cp throughputs.txt throughputs5.txt
 
    rm simgraph.jpg
 
@@ -59,21 +72,61 @@ do
    then
       echo Simulation in hypercube topology is finished.
       cp simgraph.jpg simgraph_hypercube_run${RUNNUMBER}.jpg
+      
+      cp results1.xml results_hypercube_ecmp${RUNNUMBER}.xml
+      cp results2.xml results_hypercube_timp${RUNNUMBER}.xml
+      cp results3.xml results_hypercube_contra${RUNNUMBER}.xml
+      
+      cp throughputs1.txt throughputs_hypercube_ecmp${RUNNUMBER}.txt
+      cp throughputs2.txt throughputs_hypercube_timp${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_hypercube_contra${RUNNUMBER}.txt
    fi
    if [ $1 -eq 2 ]
    then
       echo Simulation in flattened butterfly topology is finished.
       cp simgraph.jpg simgraph_flattened_butterfly_run${RUNNUMBER}.jpg
+      
+      cp results1.xml results_flattened_butterfly_ecmp${RUNNUMBER}.xml
+      cp results2.xml results_flattened_butterfly_timp${RUNNUMBER}.xml
+      cp results3.xml results_flattened_butterfly_contra${RUNNUMBER}.xml
+      
+      cp throughputs1.txt throughputs_flattened_butterfly_ecmp${RUNNUMBER}.txt
+      cp throughputs2.txt throughputs_flattened_butterfly_timp${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_flattened_butterfly_contra${RUNNUMBER}.txt
+      
    fi
    if [ $1 -eq 3 ]
    then
       echo Simulation in torus topology is finished.
       cp simgraph.jpg simgraph_torus_run${RUNNUMBER}.jpg
+   
+      cp results1.xml results_torus_ecmp${RUNNUMBER}.xml
+      cp results2.xml results_torus_timp${RUNNUMBER}.xml
+      cp results3.xml results_torus_contra${RUNNUMBER}.xml
+      
+      cp throughputs1.txt throughputs_torus_ecmp${RUNNUMBER}.txt
+      cp throughputs2.txt throughputs_torus_timp${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_torus_contra${RUNNUMBER}.txt
+      
    fi
    if [ $1 -eq 5 ]
    then
       echo Simulation in leaf spine topology is finished.
       cp simgraph.jpg simgraph_leafspine_run${RUNNUMBER}.jpg
+      
+      cp results1.xml results_leafspine_ecmp${RUNNUMBER}.xml
+      cp results2.xml results_leafspine_timp${RUNNUMBER}.xml
+      cp results3.xml results_leafspine_contra${RUNNUMBER}.xml
+      cp results4.xml results_leafspine_dashpkt${RUNNUMBER}.xml
+      cp results5.xml results_leafspine_dashflowlet${RUNNUMBER}.xml
+      
+      
+      cp throughputs1.txt throughputs_leafspine_ecmp${RUNNUMBER}.txt
+      cp throughputs2.txt throughputs_leafspine_timp${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_leafspine_contra${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_leafspine_dashpkt${RUNNUMBER}.txt
+      cp throughputs3.txt throughputs_leafspine_dashflowlet${RUNNUMBER}.txt
+      
    fi
 
    if [ $1 -eq 1 ] || [ $1 -eq 2 ] || [ $1 -eq 3 ];
@@ -146,5 +199,6 @@ then
    fi
 
 fi
+
 
 
